f7b36fe8b51800089d7fdd178d75720c8344f3ee
carllix <carloangkisan21@gmail.com>
2026-01-05 17:38:31 +0700

    chore: docker setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,6 +9,13 @@ import (
 func main() {
     app := fiber.New()
 
+    app.Get("/health", func(c *fiber.Ctx) error {
+        return c.JSON(fiber.Map{
+            "status": "ok",
+            "service": "matchaciee-api",
+        })
+    })
+
     app.Get("/", func (c *fiber.Ctx) error {
         return c.SendString("Hello, World!")
     })
