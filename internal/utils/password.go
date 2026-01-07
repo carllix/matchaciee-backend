4a5572de5bedebb642d7741df636c915549fba97
carllix <carloangkisan21@gmail.com>
2026-01-07 22:53:47 +0700

    feat: add password utils

---
+++ internal/utils/password.go
@@ -0,0 +1,34 @@
+package utils
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var (
+	ErrInvalidPassword = errors.New("invalid password")
+)
+
+const (
+	DefaultCost = 12
+)
+
+func HashPassword(password string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(password), DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hashedBytes), nil
+}
+
+func ComparePassword(hashedPassword, password string) error {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return ErrInvalidPassword
+		}
+		return err
+	}
+	return nil
+}
